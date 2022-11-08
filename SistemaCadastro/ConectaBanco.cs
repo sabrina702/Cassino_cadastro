fd63bd177f1c2b4bd57a19c51c706b3e4aced66e
sabrina silva <sabrina.1232022@outlook.com>
2022-11-08 10:12:26 -0300

    adicionando bandas

---
+++ SistemaCadastro/ConectaBanco.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace SistemaCadastro
+{
+    internal class ConectaBanco
+    {
+        MySqlConnection conexao = new MySqlConnection("server=localhost;user id=root;database=banco_cadastro");
+        public String mensagem;
+        public DataTable listaGeneros()
+        {
+            // comentario
+            MySqlCommand cmd = new MySqlCommand("lista_generos", conexao);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                conexao.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable tabela = new DataTable();
+                da.Fill(tabela);
+                return tabela;
+            }// fim try
+            catch (MySqlException e)
+            {
+                mensagem = "Erro:" + e.Message;
+                return null;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }// fim lista_generos
+
+        public bool insereBanda(Banda b)
+        {
+            MySqlCommand cmd = new MySqlCommand("insere_banda", conexao);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("nome", b.Nome);
+            cmd.Parameters.AddWithValue("genero", b.Genero);
+            cmd.Parameters.AddWithValue("integrantes", b.Integrantes);
+            cmd.Parameters.AddWithValue("ranking", b.Ranking);
+            try
+            {
+                conexao.Open();
+                cmd.ExecuteNonQuery(); // executa o comando
+                return true;
+            }
+            catch (MySqlException e)
+            {
+                mensagem = "Erro:" + e.Message;
+                return false;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }// fim insereBanda
+
+
+
+
+
+
+    }
+}
---
+++ SistemaCadastro/Sistema.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SistemaCadastro
+{
+    public partial class Sistema : Form
+    {
+
+        public Sistema()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCadastra_Click(object sender, EventArgs e)
+        {
+            marcador.Height = btnCadastra.Height;
+            marcador.Top = btnCadastra.Top;
+            tabControl1.SelectedTab = tabControl1.TabPages[0];
+        }
+        
+
+        private void btnBusca_Click(object sender, EventArgs e)
+        {
+            marcador.Height = btnBusca.Height;
+            marcador.Top = btnBusca.Top;
+            tabControl1.SelectedTab = tabControl1.TabPages[1];
+        }
+
+
+
+
+
+
+
+        private void Sistema_Load(object sender, EventArgs e)
+        {
+            ConectaBanco con = new ConectaBanco();
+            DataTable tabelaDados = new DataTable();
+            tabelaDados = con.listaGeneros();
+            cbGenero.DataSource = tabelaDados;
+            cbGenero.DisplayMember = "genero";
+            cbGenero.ValueMember = "idgenero";
+            lblmsgerro.Text = con.mensagem;
+            cbGenero.Text = "";
+        }
+
+
+        private void BtnConfirmaCadastro_Click_1(object sender, EventArgs e)
+        {
+            Banda b = new Banda();
+            b.Nome = txtnome.Text;
+            b.Genero =Convert.ToInt32(cbGenero.SelectedValue.ToString());
+            b.Integrantes = Convert.ToInt32(txtintegrantes.Text);
+            b.Ranking = Convert.ToInt32(txtranking.Text);
+            // enviar para o banco 
+            ConectaBanco conecta = new ConectaBanco();
+            bool retorno = conecta.insereBanda(b);
+            if (retorno == true)
+            {
+                MessageBox.Show("Dados inseridos com sucesso!");
+            }
+            else
+                lblmsgerro.Text = conecta.mensagem;
+
+        }
+
+        private void txtBusca_TextChanged(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void btnRemoveBanda_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+         private void btnConfirmaAlteracao_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void bntAddGenero_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
